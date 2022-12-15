b60c612d2d0e9eb3137bab7a654cdeedf057981b
Visakh M V <visakhsajong@gmail.com>
2022-12-15 15:50:02 +0530

    Half

--- src/Pages/TicketBooking/Pages/HomePage/DotHomePage.js
+++ src/Pages/TicketBooking/Pages/HomePage/DotHomePage.js
@@ -66,9 +66,52 @@ const navigateToUser = () => {
         <div className="bannerboxcontent2">Book your favourite seat now.</div>
     </div>
     <br />
-      <h1>hello</h1>
+      <div className="dotboxes">
+        <div className="dotbox1">
+        <span className='dotbox1Text'>Save upto Rs 300 on Ap and TS route</span>
+        <img src="https://st.redbus.in/images/offers/superhit_rav/1_1.png" alt="" />
+        <span className='dotbox1Text2'>Use Code SUPERHIT</span>
+        </div>
+        <div className="dotbox2">
+        <span className='dotbox2Text'>Save upto Rs 300 on Ap and TS route</span>
+        <img src="https://st.redbus.in/Images/INDOFFER/RB200/274x148.png" alt="" />
+        <span className='dotbox2Text2'>Use Code SUPERHIT</span>
+        </div>
+      </div>
+    </div>
+    <div className="dotsection2">
+      <img className='dotsectionimg' src="https://mmt.servedbyadbutler.com/getad.img/;libID=3436743" alt="" />
+      <div className="dotsectioncontainer">
+        <span className='dotheading1'>CONVENIENCE ON-THE-GO.</span>
+        <p>Enjoy the following exclusive features on the redBus app <br />
+        <br />
+Last Minute Booking - In a hurry to book a bus at the last minute?<br /> Choose the bus passing from your nearest boarding point and book  <br />
+Boarding Point Navigation - Never lose your way while travelling to <br />
+Comprehensive Ticket Details- Everything that you need to make the 
+<br /> <br />
+Enter your mobile number below to download the redBus mobile app.</p>
+  <div className="dotinput">
+  <select >
+  <option value="91">91</option>
+  <option value="36">36</option>
+  <option value="11">11</option>
+  </select>
+  <input type="text" className='dotinput1' placeholder='Enter Your Mobile Number' /><br />
+  <button className='dotbutton'>SMS ME THE LINK</button>
+      </div>
+      <div className="dotphoneimg">
+        <img className='dotphoneimg1' src="https://s1.rdbuz.com/web/images/home/IOS_Android_device.png" alt="" />
+      </div>
+      </div>
+    </div>
+    <div className="dotsection3">
+    <img className="dotsection3icon" src="https://s1.rdbuz.com/web/images/home/promise.png" alt="" />
+    <div className='dotsection3Text'>WE PROMISE TO DELIVER</div>
+    <div className="dotsectioncards">
+      
     </div>
 
+    </div>
   </div>
   )
 }
